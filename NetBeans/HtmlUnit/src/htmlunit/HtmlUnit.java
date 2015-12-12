8ac843ed8a0491bd9e7bc306ec900eb743854f71
JLRoper <JacobLRoper@gmail.com>
2015-12-12 16:26:56 -0500

    testFacebook

--- NetBeans/HtmlUnit/src/htmlunit/HtmlUnit.java
+++ NetBeans/HtmlUnit/src/htmlunit/HtmlUnit.java
@@ -5,6 +5,13 @@
  */
 package htmlunit;
 
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Jacob
@@ -16,6 +23,16 @@ public class HtmlUnit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        try (final WebClient webClient = new WebClient()) {
+            final HtmlPage page = webClient.getPage("https://www.facebook.com");
+            System.out.println(page.getTitleText());
+        } catch (IOException ex) {
+            Logger.getLogger(HtmlUnit.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (FailingHttpStatusCodeException ex) {
+            Logger.getLogger(HtmlUnit.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-    
+
 }
