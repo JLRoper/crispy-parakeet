294e3f1b564bc5fa1e1c9c1eb5c72bdc650236e7
JLRoper <Jacob@DESKTOP-KU1J8OI>
2018-10-05 22:18:47 -0400

    jframe/panel test

---
+++ NetBeansProjects/JSPPractice/src/java/Servlet/JamesTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlet;
+
+import java.awt.Component;
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicArrowButton;
+
+/**
+ *
+ * @author Jacob
+ */
+public class JamesTest {
+
+    public static void main(String args[]) {
+    JFrame framePanel = new JFrame();
+    framePanel.setVisible(true);
+
+    Component comp = new BasicArrowButton(3);
+    framePanel.setComponentZOrder(comp, 0);
+
+    }
+}
